3cfa7f033b560d8dfa337f159983f68577c37dd5
AliValizade <alivalizade@gmail.com>
2024-06-23 02:09:50 +0330

    improve LLS

--- Assignment_44/modules.py
+++ Assignment_44/modules.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numpy.linalg import inv
 import matplotlib.pyplot as plt
 
 
@@ -8,22 +9,34 @@ class LLS:
 
     # Train
     def fit(self, X, y):
-        X = X.reshape(-1, 1)
-        self.w = np.matmul(np.matmul(np.linalg.inv(np.matmul(X.T, X)), X.T), y)
+        self.w = inv(X.T @ X) @ X.T @ y
 
     # Predict
     def predict(self, X):
-        X = X.reshape(-1, 1)
-        return self.w * X
+        return X @ self.w
+    
+    def evaluate(self, X_test, Y_test, metric):
+        Y_pred  =self.predict(X_test)
+        error = Y_test - Y_pred
+
+        if metric == 'mae':
+            loss = np.sum(np.abs(error)) / len(Y_test)
+        elif metric == 'mse':
+            loss = np.sum(error ** 2) / len(Y_test)
+
+        return loss
 
     # Show Result
-    def plot(self, X, y, color, xlabel, ylabel):
-        plt.scatter(X, y)
-        plt.plot(X, self.predict(X), color=color)
+    def plot(self, X_train, X_test, Y_train, Y_test, color_train, color_test, xlabel, ylabel):
+        plt.scatter(X_train, Y_train, c=color_train, label='Training data', alpha=0.5)
+        plt.scatter(X_test, Y_test, c=color_test, label='Testing data', alpha=0.5)
+        plt.plot(X_test, self.predict(X_test), color='green', label='Prediction')
         plt.xlabel(xlabel)
         plt.ylabel(ylabel)
         plt.title('Linear Regression')
+        plt.legend()
         plt.show()
+
 
 
 class LLS_3D:
@@ -33,10 +46,10 @@ class LLS_3D:
     # Train
     def fit(self, X, y):
         X_b = np.hstack([np.ones((X.shape[0], 1)), X])    # Add bias column
-        self.w = np.matmul(np.matmul(np.linalg.inv(np.matmul(X_b.T, X_b)), X_b.T), y)
+        self.w = inv(X_b.T @ X_b) @ X_b.T @ y
 
     # Predict
     def predict(self, X):
         X_b = np.hstack([np.ones((X.shape[0], 1)), X])    # Add bias column
-        return X_b.dot(self.w)
+        return X_b @ self.w
     
