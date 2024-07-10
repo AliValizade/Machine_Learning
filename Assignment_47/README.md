fc44339734d1d6daaf028c8652c18dae804488eb
AliValizade <alivalizade@gmail.com>
2024-07-10 22:14:13 +0330

    first commit

---
+++ Assignment_47/README.md
@@ -0,0 +1,18 @@
+<p style="color: orange; font-weight: bold; font-size: 18px">Assinment-47 (Machine Learning) Neural Networks</p>
+<br>
+
+- <p style="color: lightgreen; font-size: 18px; font-family: Tahoma">Perceptron (Activation Functions)</p>
+- <p style="color: lightgreen; font-size: 18px; font-family: Tahoma">Surgical 🏨 </p>
+
+![](data)
+
+![](data)
+
+
+<br>
+
+- <p style="color: lightgreen; font-size: 18px; font-family: Tahoma"> 🌦پیش بینی آب و هوا </p>
+
+
+![](data)
+
---
+++ Assignment_47/modules.py
@@ -0,0 +1,52 @@
+import numpy as np
+from tqdm import tqdm
+
+
+class Perceptron:
+    def __init__(self, input_size, lr_w=0.001, lr_b=0.01, epoch=0, loss_threshold=0.3):
+        self.w = np.random.rand(input_size, 1)
+        self.b = np.random.rand(1, 1)
+        self.lr_w = lr_w
+        self.lr_b = lr_b
+        self.loss_threshold = loss_threshold
+        self.stop_condition = False
+        self.Epoch = epoch
+        self.losses = []
+        self.prev_loss = float('inf')  # Store the previous loss for comparison
+
+    def activation(self, x, function):
+        if function == 'sigmoid':
+            return 1 / (1 + np.exp(-x))
+        
+
+    def fit(self, X_train, Y_train):
+        while not tqdm(self.stop_condition):
+            self.Epoch += 1
+            epoch_losses = []  # List to store losses for the current epoch
+            for x, y in zip(X_train, Y_train):
+                y_pred = x @ self.w + self.b
+                error = y - y_pred
+
+                self.w += self.lr_w * error * x
+                self.b += self.lr_b * error
+
+                loss = np.mean(np.abs(error))
+                epoch_losses.append(loss)
+            
+            mean_loss = np.mean(epoch_losses)
+            self.losses.append(mean_loss)
+            print(f"Epoch {self.Epoch}, Loss: {mean_loss}")
+
+            # Check stopping condition
+            if mean_loss < self.loss_threshold or np.abs(self.prev_loss - mean_loss) < 0.00001:
+                self.stop_condition = True
+
+            self.prev_loss = mean_loss
+
+    def predict(self, X_test):
+        return np.dot(X_test, self.w) + self.b
+
+    def evaluate(self, X_test, Y_test):
+        Y_pred = self.predict(X_test)
+        loss = np.mean(np.abs(Y_test - Y_pred))
+        return loss
