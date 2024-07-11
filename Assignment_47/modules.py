3e83de8980228a6b321d5f3ddf65a05fcdf64a41
AliValizade <alivalizade@gmail.com>
2024-07-11 20:57:09 +0330

    finish code with Mr. Aemmi

--- Assignment_47/modules.py
+++ Assignment_47/modules.py
@@ -3,7 +3,7 @@ from tqdm import tqdm
 
 
 class Perceptron:
-    def __init__(self, input_size, lr_w=0.001, lr_b=0.01, epoch=0, loss_threshold=0.3):
+    def __init__(self, input_size, lr_w=0.001, lr_b=0.01, epoch=0, loss_threshold=0.1):
         self.w = np.random.rand(input_size, 1)
         self.b = np.random.rand(1, 1)
         self.lr_w = lr_w
@@ -17,17 +17,29 @@ class Perceptron:
     def activation(self, x, function):
         if function == 'sigmoid':
             return 1 / (1 + np.exp(-x))
-        
+        elif function == 'relu':
+            return np.maximum(0, x)
+        elif function == 'tanh':
+            return np.tanh(x)
+        elif function == 'linear':
+            return x
+        else:
+            raise Exception('Unknown activation function!')
 
     def fit(self, X_train, Y_train):
-        while not tqdm(self.stop_condition):
+        while not self.stop_condition:
             self.Epoch += 1
             epoch_losses = []  # List to store losses for the current epoch
             for x, y in zip(X_train, Y_train):
-                y_pred = x @ self.w + self.b
-                error = y - y_pred
+                x = x.reshape(-1, 1)
+                y = y.reshape(-1, 1)
+                # Forwarding
+                y_pred = x.T @ self.w + self.b
+                y_pred = self.activation(y_pred, 'sigmoid')
 
-                self.w += self.lr_w * error * x
+                # Back propagation
+                error = y - y_pred
+                self.w += self.lr_w * x * error
                 self.b += self.lr_b * error
 
                 loss = np.mean(np.abs(error))
@@ -38,15 +50,38 @@ class Perceptron:
             print(f"Epoch {self.Epoch}, Loss: {mean_loss}")
 
             # Check stopping condition
-            if mean_loss < self.loss_threshold or np.abs(self.prev_loss - mean_loss) < 0.00001:
+            if mean_loss < self.loss_threshold or np.abs(self.prev_loss - mean_loss) < 0.000001:
                 self.stop_condition = True
 
             self.prev_loss = mean_loss
 
     def predict(self, X_test):
-        return np.dot(X_test, self.w) + self.b
+        Y_pred = []
+        for x_test in X_test:
+            y_pred = x_test @ self.w + self.b
+            y_pred = self.activation(y_pred, 'sigmoid')
+            Y_pred.append(y_pred)
+        return np.array(Y_pred).reshape(-1)
+    
+    def calculate_loss(self, X_test, Y_test, metric):
+        Y_pred = self.predict(X_test)
+        error = Y_test - Y_pred
+        if metric == 'mae':
+            return np.mean(np.abs(error))
+        elif metric == 'mse':
+            return np.mean(error ** 2)
+        elif metric == 'rmse':
+            return np.sqrt(np.mean(error ** 2))
+        else:
+            raise Exception('Unknown metric')
+        
+    def calculate_accuracy(self, X_test, Y_test):
+        Y_pred = self.predict(X_test)
+        Y_pred = np.where(Y_pred > 0.5, 1, 0)
+        Y_test = Y_test.reshape(-1)
+        return np.sum(Y_pred == Y_test) / len(Y_test)
 
     def evaluate(self, X_test, Y_test):
-        Y_pred = self.predict(X_test)
-        loss = np.mean(np.abs(Y_test - Y_pred))
-        return loss
+        loss = self.calculate_loss(X_test, Y_test, 'mse')
+        accuracy = self.calculate_accuracy(X_test, Y_test)
+        return loss, accuracy
